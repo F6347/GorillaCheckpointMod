4f62054dded488329e7c38428534ad4f25b255a4
F6347 <federcraft@outlook.es>
2024-11-06 22:17:44 -0500

    Add project files.

---
+++ GorillaCheckpointMod/Plugin.cs
@@ -0,0 +1,185 @@
+﻿using BepInEx;
+using Newtilla;
+using UnityEngine;
+using GorillaExtensions;
+using UnityEngine.InputSystem;
+using System.Threading.Tasks;
+
+namespace GorillaCheckpointMod
+{
+    [BepInDependency("Lofiat.Newtilla", "1.0.1")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        GameObject point;
+        bool isPressingDown;
+        Color VRRig_Color;
+
+        void Start()
+        {
+            Newtilla.Newtilla.OnJoinModded += OnModdedJoined;
+            Newtilla.Newtilla.OnLeaveModded += OnModdedLeft;
+        }
+
+        void OnEnable() => HarmonyPatches.ApplyHarmonyPatches();
+        void OnDisable()
+        {
+            Destroy(point);
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void Update()
+        {
+            if (!inRoom) return;
+
+            if (Keyboard.current.yKey.isPressed || ControllerInputPoller.instance.leftControllerSecondaryButton)
+            {
+                if (!isPressingDown)
+                    point.transform.position = GorillaLocomotion.Player.Instance.headCollider.transform.position;
+            }
+            else if (Keyboard.current.bKey.isPressed || ControllerInputPoller.instance.rightControllerSecondaryButton)
+            {
+                if (!isPressingDown)
+                {
+                    TeleportPlayer(point.transform.position);
+
+                }
+            }
+
+            isPressingDown = Keyboard.current.yKey.isPressed || Keyboard.current.bKey.isPressed ||
+                             ControllerInputPoller.instance.leftControllerSecondaryButton ||
+                             ControllerInputPoller.instance.rightControllerSecondaryButton;
+        }
+
+        void OnModdedJoined(string modeName)
+        {
+            point = CreateCheckpoint();
+            inRoom = true;
+        }
+
+        void OnModdedLeft(string modeName)
+        {
+            Destroy(point);
+            inRoom = false;
+        }
+
+        GameObject CreateCheckpoint()
+        {
+            var checkpoint = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var material = new Material(Shader.Find("Universal Render Pipeline/Lit"))
+            {
+                color = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player").GetComponent<VRRig>().playerColor
+            };
+            checkpoint.GetComponent<Renderer>().material = material;
+            checkpoint.GetComponent<BoxCollider>().enabled = false;
+            checkpoint.transform.localScale = new Vector3(0.25f, 0.5f, 0.25f);
+            return checkpoint;
+        }
+
+        void TeleportPlayer(Vector3 checkpointPos)
+        {
+            foreach (var collider in Resources.FindObjectsOfTypeAll<MeshCollider>())
+                collider.enabled = false;
+            GorillaLocomotion.Player.Instance.headCollider.transform.position = checkpointPos;
+            Task.Run(async () =>
+            {
+                await Task.Delay(1);
+                foreach (var collider in Resources.FindObjectsOfTypeAll<MeshCollider>())
+                    collider.enabled = true;
+            });
+        }
+    }
+}using BepInEx;
+using Newtilla;
+using UnityEngine;
+using GorillaExtensions;
+using UnityEngine.InputSystem;
+using System.Threading.Tasks;
+
+namespace GorillaCheckpointMod
+{
+    [BepInDependency("Lofiat.Newtilla", "1.0.1")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        GameObject point;
+        bool isPressingDown;
+        Color VRRig_Color;
+
+        void Start()
+        {
+            Newtilla.Newtilla.OnJoinModded += OnModdedJoined;
+            Newtilla.Newtilla.OnLeaveModded += OnModdedLeft;
+        }
+
+        void OnEnable() => HarmonyPatches.ApplyHarmonyPatches();
+        void OnDisable()
+        {
+            Destroy(point);
+            HarmonyPatches.RemoveHarmonyPatches();
+        }
+
+        void Update()
+        {
+            if (!inRoom) return;
+
+            if (Keyboard.current.yKey.isPressed || ControllerInputPoller.instance.leftControllerSecondaryButton)
+            {
+                if (!isPressingDown)
+                    point.transform.position = GorillaLocomotion.Player.Instance.headCollider.transform.position;
+            }
+            else if (Keyboard.current.bKey.isPressed || ControllerInputPoller.instance.rightControllerSecondaryButton)
+            {
+                if (!isPressingDown)
+                {
+                    TeleportPlayer(point.transform.position);
+
+                }
+            }
+
+            isPressingDown = Keyboard.current.yKey.isPressed || Keyboard.current.bKey.isPressed ||
+                             ControllerInputPoller.instance.leftControllerSecondaryButton ||
+                             ControllerInputPoller.instance.rightControllerSecondaryButton;
+        }
+
+        void OnModdedJoined(string modeName)
+        {
+            point = CreateCheckpoint();
+            inRoom = true;
+        }
+
+        void OnModdedLeft(string modeName)
+        {
+            Destroy(point);
+            inRoom = false;
+        }
+
+        GameObject CreateCheckpoint()
+        {
+            var checkpoint = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            var material = new Material(Shader.Find("Universal Render Pipeline/Lit"))
+            {
+                color = GameObject.Find("Player Objects/Local VRRig/Local Gorilla Player").GetComponent<VRRig>().playerColor
+            };
+            checkpoint.GetComponent<Renderer>().material = material;
+            checkpoint.GetComponent<BoxCollider>().enabled = false;
+            checkpoint.transform.localScale = new Vector3(0.25f, 0.5f, 0.25f);
+            return checkpoint;
+        }
+
+        void TeleportPlayer(Vector3 checkpointPos)
+        {
+            foreach (var collider in Resources.FindObjectsOfTypeAll<MeshCollider>())
+                collider.enabled = false;
+            GorillaLocomotion.Player.Instance.headCollider.transform.position = checkpointPos;
+            Task.Run(async () =>
+            {
+                await Task.Delay(1);
+                foreach (var collider in Resources.FindObjectsOfTypeAll<MeshCollider>())
+                    collider.enabled = true;
+            });
+        }
+    }
+}
---
+++ GorillaCheckpointMod/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaCheckpointMod
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.f6347.gorillatag.gorillacheckpointmod";
+        public const string Name = "GorillaCheckpointMod";
+        public const string Version = "1.0.0";
+    }
+}
